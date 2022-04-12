1632ff555b9d5f59249f67423ae104ee1f98d97a
Daniel A Perez <danielpabraham97@gmail.com>
2022-04-12 23:54:05 +0000

    new code

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todoArray, setTodoArray] = useState([]);
+	useEffect(() => getData(), []);
+
+	const getData = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/danielpabraham")
+			.then((response) => {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				return response.json();
+			})
+			.then((responseAsJson) => {
+				setTodoArray(responseAsJson);
+			})
+			.catch((error) => {
+				console.log("There is an error in this code: /n", error);
+			});
+	};
+
+	let mappedTodoArray = todoArray.map((task, index) => {
+		return (
+			<li>
+				{task.label}
+				<button onClick={() => removeTask(index)}>
+					<i class="fas fa-times"></i>
+				</button>{" "}
+			</li>
+		);
+	});
+
+	function removeTask(i) {
+		let filtered = todoArray.filter((task, index) => i != index);
+
+		setTodoArray(filtered);
+	}
+
+	function removeAll() {
+		setTodoArray([]);
+	}
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="todoBox">
+			<div className="header">
+				<h1>To-Do</h1>
+				<button className="clearAll" onClick={() => removeAll()}>
+					{" "}
+					Clear All{" "}
+				</button>
+			</div>
+			<div className="inputField">
+				<input
+					id="newTask"
+					type="text"
+					placeholder="..."
+					value={inputValue}
+					onChange={(e) => setInputValue(e.target.value)}
+					onKeyUp={(e) => {
+						if (e.key === "Enter") {
+							setTodoArray([
+								...todoArray,
+								{
+									label: inputValue,
+									done: false,
+								},
+							]);
+							setInputValue("");
+						}
+					}}
+				/>
+			</div>
+			<div>
+				<ul>{mappedTodoArray}</ul>
+			</div>
 		</div>
 	);
 };
